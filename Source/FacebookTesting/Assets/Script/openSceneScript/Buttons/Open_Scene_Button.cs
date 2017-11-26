a84ffae5b2977d45e53fde8bba40a036516c22af
phylieng <ltphy@apcs.vn>
2017-11-26 08:57:45 +0700

    Add FacebookSDK and Shop demo
    Sharing method works well but Inviting method needs to have a real URL
    of the app on store to implement.
    Title and description will not work with ShareLink or AppLink since
    Facebook changes their way to prevent scam and fake news.
    I need more times to examine this issues

---
+++ Source/FacebookTesting/Assets/Script/openSceneScript/Buttons/Open_Scene_Button.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open_Scene_Button : MonoBehaviour {
+	public string sceneName;
+	// Use this for initialization
+	public void Create(){
+	}
+	public void OnMouseDown () {
+		SceneManager.LoadScene(sceneName);
+	}
+}
