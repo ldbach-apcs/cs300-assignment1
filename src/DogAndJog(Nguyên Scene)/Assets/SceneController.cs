320b7ae194bb1946145d54da7e0844d590a694ed
hsn231 <hsnguyen@apcs.vn>
2017-12-27 19:20:57 +0700

    Nguyên Main Scene

---
+++ src/DogAndJog(Nguyên Scene)/Assets/SceneController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+
+	public void LoadScene(string Scene){
+		Application.LoadLevel (Scene);
+	}
+
+	public void OpenMenu(GameObject obj){
+		obj.SetActive (!obj.activeInHierarchy);
+	}
+}
