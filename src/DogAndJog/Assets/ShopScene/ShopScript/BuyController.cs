b9ece33e56bb4959bb91a17159abd7663816e48f
ldbach-apcs <ldbach@apcs.vn>
2017-12-28 09:44:58 +0700

    Rearragne

---
+++ src/DogAndJog/Assets/ShopScene/ShopScript/BuyController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyController : MonoBehaviour {
+    //Loading and static call
+    public static BuyController buyController;
+
+    Item item;
+    int quantity, price, cost;
+    public GameObject ItemView, QuantityView;//, button;
+    Text text;
+    public void Start()
+    {
+        text = QuantityView.transform.GetChild(0).GetComponent<Text>();
+        text.text = "1";
+        buyController = this;
+        SetNonActive();
+    }
+    public void SetNonActive()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(false);
+        CoinUI.coinUI.LoadInfo();
+    }
+
+    // Use this for initialization
+    public void ItemLoad(Item item)
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(1).gameObject.SetActive(true);
+        this.item = item;
+        quantity = 1;
+        if (item.GetQuantiable())
+        {
+            QuantityView.SetActive(true);
+            quantity = 1;
+            text.text = "1";
+        }
+        else QuantityView.SetActive(false);
+        price = item.GetPrice();
+        price = item.GetPrice();
+        cost = price;
+    }
+
+    public void IncreaseQuantity(int a)
+    {
+        quantity += a;
+        if (quantity < 1) quantity = 1;
+        if (quantity > 99) quantity = 99;
+        cost = price * quantity;
+        text.text = quantity.ToString();
+    }
+	public void Buy()
+    {
+        Currency.Decrease(cost);
+        SetNonActive();
+    }
+}
---
+++ src/DogAndJog/Assets/ShopScene/ShopScript/Scroll_Dynamic_Content.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using UnityEngine.UI;
+
+public class Scroll_Dynamic_Content : MonoBehaviour {
+    string conn;
+    string type;
+    public GameObject prefab;
+    public int itemHeight;
+    IDbConnection dbconn;
+    IDbCommand dbcmd;
+    IDataReader reader;
+    Vector3 position = Vector3.up * 780;
+    void Start()
+    {
+        LoadList("Food");
+        Rect rect = GetComponent<RectTransform>().rect;
+    }
+
+    GameObject NewUnit()
+    {
+        GameObject go = Instantiate(prefab, transform);
+        go.GetComponent<RectTransform>().position = position + Vector3.down * itemHeight * (transform.childCount + 1);// + new Vector3(0, -transform.childCount * 140, 0);
+        string name = reader.GetString(0);
+        string description = reader.GetString(1);
+        int storeCost = reader.GetInt32(2);
+        Item item = null;
+        if (type.Equals("Food")) item = new MyFood(name, description, storeCost);
+        if (type.Equals("Skin")) item = new MySkin(name, description, storeCost);
+        go.GetComponent<StoreItemController>().SetItem(item);
+        return go;
+    }
+
+    void OpenConnect()
+    {
+        conn = "URI=file:" + Application.dataPath + "/database.db"; //Path to database.
+
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open();
+        dbcmd = dbconn.CreateCommand();
+    }
+    void CloseConnect()
+    {
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+
+    public void LoadList(string type)
+    {
+        Transform tf;
+        for (int i = transform.childCount - 1; i>=0; i--)
+        {
+            tf = transform.GetChild(i);
+            tf.parent = null;
+            Destroy(tf.gameObject);
+        }
+        this.type = type;
+        OpenConnect();
+        string sqlQuery = "SELECT name, description, storeCost FROM " + type;
+        dbcmd.CommandText = sqlQuery;
+        reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            NewUnit();
+            /*string name = reader.GetString(0);
+            string description = reader.GetString(1);
+            int storeCost = reader.GetInt32(2);
+			Debug.Log( "name ="+name+"  description ="+  description);*/
+        }
+        CloseConnect();
+        GetComponent<RectTransform>().sizeDelta = new Vector2(0, transform.childCount * itemHeight);
+    }
+}
