77e8b6cc93f6644a163006bbfff805aeb4cbc09a
hsn231 <hsnguyen@apcs.vn>
2017-12-29 21:40:43 +0700

    Nguyên

---
+++ src/DogAndJog/Assets/MainScene/SkinSelect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinSelect : MonoBehaviour {
+
+	public Texture[] textures;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().materials [0].mainTexture = textures [DatabaseReader.Instance().currentSkin];
+	}
+}
